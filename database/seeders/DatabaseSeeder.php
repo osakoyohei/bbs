40a6d51e99a2c57bc69544f6f70ce087956e4591
osakoyohei <sako4.nsr@gmail.com>
2021-09-14 01:49:28 +0900

    ゲストログイン実装

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        $this->call(CategoriesTableSeeder::class);
+        // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            CategoriesTableSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
