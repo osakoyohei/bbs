92693d0d2a186018dd7af235bd650a2a8859b375
osakoyohei <sako4.nsr@gmail.com>
2021-09-14 02:05:28 +0900

    seeder編集

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -14,32 +14,19 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
-        DB::table('categories')->truncate();
-        
         DB::table('categories')->insert([
             [
                 'id' => '1',
                 'name' => 'カテゴリ−1',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
                 'id' => '2',
                 'name' => 'カテゴリ−2',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
                 'id' => '3',
                 'name' => 'カテゴリ−3',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
             ],
         ]);
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
     }
 }
