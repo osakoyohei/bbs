4efe1b407370434a55e40317853d760775080c88
osakoyohei <sako4.nsr@gmail.com>
2021-12-28 05:55:32 +0900

    adminseeder追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriesTableSeeder::class,
             UserSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
